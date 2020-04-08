a69be51630b2a212aecb397d9137aff9430577de
Maysam <maysam82.dev@gmail.com>
2020-04-08 19:58:43 +1000

    add graphql to app and setup schema with mock data

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
-
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 const app = express();
+
+// create a middleware as a single route. and this route will be like an end point to interact with endpoint data . graphqlHTTP will be called as soon as getting a request to /graphql route
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		// shcema should be passed here
+		schema,
+	})
+);
 
 app.listen(4000, () => {
 	console.log('Listening for requests on port 4000');
---
+++ server/schema/schema.js
@@ -0,0 +1,41 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+var books = [
+	{ name: 'sample book 1', genre: 'sample genre 1', id: '1' },
+	{ name: 'sample book 2', genre: 'sample genre 2', id: '2' },
+	{ name: 'sample book 3', genre: 'sample genre 3', id: '3' },
+];
+
+// define a new object type "BookType"
+const BookType = new GraphQLObjectType({
+	name: 'Book',
+	fields: () => ({
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		genre: { type: GraphQLString },
+	}),
+});
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		// it is going to be name of a query which we use in the client side
+		book: {
+			type: BookType,
+			args: {
+				id: { type: GraphQLString },
+			},
+			resolve(parents, args) {
+				// code to get data from db / other sources. How gets data while someone is making a request
+				return books.find(({ id }) => (id = args.id));
+			},
+		},
+	},
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery,
+});
