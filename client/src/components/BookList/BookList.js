5b22c0d7b720352dce98ff51acf65d3eedeaf0ad
Maysam <maysam82.dev@gmail.com>
2020-04-09 19:00:45 +1000

    fetch books and render them in component

--- client/src/components/BookList/BookList.js
+++ client/src/components/BookList/BookList.js
@@ -11,12 +11,24 @@ const getbookList = gql`
 	}
 `;
 class BookList extends Component {
+	fetchBooks = () => {
+		const {
+			data: { loading, books },
+		} = this.props;
+		if (loading) {
+			return <div className="loading">Loading Data ... </div>;
+		} else {
+			return books.map(({ name, id }) => (
+				<li className="book" key={id}>
+					{name}
+				</li>
+			));
+		}
+	};
 	render() {
 		return (
 			<div className="book-list-container">
-				<ul className="book-list">
-					<li>book name</li>
-				</ul>
+				<ul className="book-list">{this.fetchBooks()}</ul>
 			</div>
 		);
 	}
