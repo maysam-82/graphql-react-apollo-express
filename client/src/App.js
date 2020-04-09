c7e23f2563345aa793433dfc918adf2ab5045659
Maysam <maysam82.dev@gmail.com>
2020-04-09 18:20:29 +1000

    create and add booklist component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import BookList from './components/BookList/BookList';
 
 function App() {
-	return <div className="App">Hello</div>;
+	return (
+		<div className="App">
+			<BookList />
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/components/BookList/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+	render() {
+		return (
+			<div className="book-list-container">
+				<ul className="book-list">
+					<li>book name</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default BookList;
