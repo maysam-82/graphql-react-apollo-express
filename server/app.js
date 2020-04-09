623f3db54c98231a9a4548233a5a5246ab00c49f
Maysam <maysam82.dev@gmail.com>
2020-04-09 18:47:12 +1000

    add cors to server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const cors = require('cors');
 const app = express();
 
+// Allow cross-origin request
+app.use(cors());
 // create a middleware as a single route. and this route will be like an end point to interact with endpoint data . graphqlHTTP will be called as soon as getting a request to /graphql route
 app.use(
 	'/graphql',
